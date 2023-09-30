0ce122b255acbf82834ff90303eefc941b97f4e4
Mahendra <mahendradewangan195@gmail.com>
2023-09-30 16:28:17 +0530

    added VideoRepositoryPage

--- help-me-out-web/src/App.jsx
+++ help-me-out-web/src/App.jsx
@@ -1,15 +1,13 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import LandingPage from "./pages/LandingPage/LandingPage"
-import Features from "./Components/LandingPage - components/Features"
-import Header from "./Components/Header/Header"
-import Works from "./Components/LandingPage - components/Works"
-import Footer from "./Components/Footer/Footer"
-import Hero from "./Components/LandingPage - components/Hero"
+import VideoRepository from "./pages/VideoRepositoryPage/VideoRepository"
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/" element={<LandingPage />} /> */}
+      <Route path="/" element={<VideoRepository />} />
       
     </Route>
   )
