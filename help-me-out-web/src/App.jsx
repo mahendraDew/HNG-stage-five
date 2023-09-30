426bf7763e476dd1553be2943b1a8303e8b56f40
Mahendra <mahendradewangan195@gmail.com>
2023-09-30 17:56:08 +0530

    added singleVideoView page

--- help-me-out-web/src/App.jsx
+++ help-me-out-web/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import VideoRepository from "./pages/VideoRepositoryPage/VideoRepository"
+import SingleVideoView from "./pages/singleVideoView/SingleVideoView"
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       {/* <Route path="/" element={<LandingPage />} /> */}
-      <Route path="/" element={<VideoRepository />} />
+      <Route path="/video" element={<VideoRepository />} />
+      <Route path="/" element={<SingleVideoView />} />
       
     </Route>
   )
