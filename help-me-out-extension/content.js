fbf41e9c92aabe67f9aba2f151de2bf86d7c9e13
Mahendra <mahendradewangan195@gmail.com>
2023-10-01 18:52:00 +0530

    updated the condition for eventdata to push blob to videoChunks

--- help-me-out-extension/content.js
+++ help-me-out-extension/content.js
@@ -22,7 +22,7 @@ function onAccessApproved(stream) {
 
         if (event.data.size > 0) {
             console.log("inside event size");
-            if (event.data.type === 'video/x-matroska;codecs=avc1') {
+            if (event.data.type.startsWith("video")) {
                 console.log("inside type condition")
                 videoChunks.push(event.data);
                 console.log("eventdata:", event.data);
