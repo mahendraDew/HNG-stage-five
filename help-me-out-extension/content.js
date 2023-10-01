af44d8a0f3ca51f41c545f9605e4581714f79ab6
Mahendra <mahendradewangan195@gmail.com>
2023-10-01 18:09:19 +0530

    updated the extension

--- help-me-out-extension/content.js
+++ help-me-out-extension/content.js
@@ -1,61 +1,112 @@
 console.log("contentjs file have been injected !!!")
 
 var recorder = null
-function onAccessApproved(stream){
+function onAccessApproved(stream) {
     recorder = new MediaRecorder(stream);
+    // Arrays to store video and audio data
+    let videoChunks = [];
+    let audioChunks = [];
+    console.log("videoChunks:", videoChunks);
 
-    recorder.start();
+    recorder.start(1000);
 
-    recorder.onstop = function(){
-        stream.getTracks().forEach(function(track){
-            if(track.readyState === "live"){
+    recorder.onstop = function () {
+        stream.getTracks().forEach(function (track) {
+            if (track.readyState === "live") {
                 track.stop()
             }
         })
     }
 
-    recorder.ondataavailable = function(event){
-        let recordedBlob  = event.data;
-        let url = URL.createObjectURL(recordedBlob);
+    recorder.ondataavailable = function (event) {
 
-        let a = document.createElement("a");
+        if (event.data.size > 0) {
+            console.log("inside event size");
+            if (event.data.type === 'video/x-matroska;codecs=avc1') {
+                console.log("inside type condition")
+                videoChunks.push(event.data);
+                console.log("eventdata:", event.data);
+            } else if (event.data.type === 'audio/webm') {
+                audioChunks.push(event.data);
+            }
+            else {
+                console.log("not match")
+            }
+        }
+        console.log("video-chunks:", videoChunks);
+        
+        // sendChunkToBackend(event.data);
 
-        a.style.display = "none";
-        a.href = url;
-        a.download = "screen-recording.webm"
+        // let recordedBlob = event.data;
+        // sendChunkToBackend(recordedBlob);
 
-        document.body.appendChild(a);
-        a.click();
+        // if (recordedBlob.type === 'video/webm') {
+        //     console.log("inside recorder if condition");
+        //     videoChunks.push(recordedBlob);
+        // } else if (recordedBlob.type === 'audio/webm') {
+        //     audioChunks.push(recordedBlob);
+        // }
 
-        document.body.removeChild(a);
 
-        URL.revokeObjectURL(url);
+
+        //________________________  this will create a link and save the webm file in local storage ______________________________
+
+        // let recordedBlob = event.data;
+        // console.log("recordedBlob has been assigned")
+        // console.log(recordedBlob)
+        // let url = URL.createObjectURL(recordedBlob);
+        // let a = document.createElement("a");
+        // a.style.display = "none";
+        // a.href = url;
+        // a.download = "screen-recording.webm"
+        // document.body.appendChild(a);
+        // a.click();
+        // document.body.removeChild(a);
+        // URL.revokeObjectURL(url);
     }
 }
 
 
-chrome.runtime.onMessage.addListener( (message, sender, sendResponse)=>{
+function sendChunkToBackend(blob) {
+    const formData = new FormData();
+    formData.append('file', blob, 'screen-recording.webm');
 
-    if(message.action === "request_recording"){
+    fetch('https://example.com/upload', {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+        .then(data => {
+            console.log('Chunk sent successfully:', data);
+        })
+        .catch(error => {
+            console.error('Error sending chunk:', error);
+        });
+}
+
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    if (message.action === "request_recording") {
         console.log("requesting recording")
 
         sendResponse(`processed: ${message.action}`);
 
         navigator.mediaDevices.getDisplayMedia({
-            audio:true,
+            audio: true,
             video: {
-                width:9999999999,
+                width: 9999999999,
                 height: 9999999999
             }
-        }).then((stream)=>{
+        }).then((stream) => {
             onAccessApproved(stream)
-        })  
+        })
     }
 
-    if(message.action === "stopvideo"){
+    if (message.action === "stopvideo") {
         console.log("stopping video");
         sendResponse(`processed: ${message.action}`);
-        if(!recorder) return console.log("no recorder")
+        if (!recorder) return console.log("no recorder")
 
         recorder.stop();
 
